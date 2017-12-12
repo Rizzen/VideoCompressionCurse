97322d9e96e6e448f4ee75d3dca5c4c81df96e73
Marcus Weaver <onemangd@gmail.com>
2017-12-12 23:51:00 +0300

    cleanup

--- Compression.py
+++ Compression.py
@@ -1,8 +1,5 @@
 import cv2
 import time
-import requests
-import VideoWriter
-import ffmpy
 
 
 # Opens video and shows it in browser
@@ -24,33 +21,6 @@ class Video(object):
                 time.sleep(var)
             return jpeg.tobytes(), success
         return None, success
-
-
-# using requests to capture video via smartphone with IPWebcam
-class IPCamVideo(object):
-
-    def __init__(self):
-        self.url = 'http://192.168.100.7:8080/shot.jpg'
-
-    def get_frame(self):
-        response = requests.get(self.url, stream=True)
-        frame = response.content
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes(), True
-
-
-class CompressedVideoWriter(object):
-
-    def __init__(self):
-        self.video = cv2.VideoCapture('E:\PyProjects\Resources\\test.mov')
-        self.writer = VideoWriter.Writer()
-
-    def __del__(self):
-        self.video.release()
-        self.writer.__del__()
-
-    def write_video(self):
-        self.writer.write(self.video)
 
 
 class WebCam(object):
@@ -75,15 +45,3 @@ def gen(video):
         if success:
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-def cap_and_write(writer):
-    writer.write_video()
-
-
-def comp_ffmpy():
-    ff = ffmpy.FFmpeg(
-        inputs={'E:\PyProjects\Resources\\test.mov': None},
-        outputs={'output.avi': None}
-    )
-    ff.run()
--- Main.py
+++ Main.py
@@ -17,34 +17,13 @@ def video_feed():
     return res
 
 
-# http://127.0.0.1:5000/ip_camera
-@app.route('/ip_camera')
-def camera():
-    res = Response(compression.gen(compression.IPCamVideo()),
-                   mimetype='multipart/x-mixed-replace; boundary=frame')
-    return res
-
-
-# http://127.0.0.1:5000/video_write
-@app.route('/video_write')
-def video_write():
-    compression.cap_and_write(compression.CompressedVideoWriter())
-    return 'Done!'
-
-
-# http://127.0.0.1:5000/ffmpy
-@app.route('/ffmpy')
-def ffmpy():
-    compression.comp_ffmpy()
-    return 'Done!'
-
-
 # http://127.0.0.1:5000/web_camera
 @app.route('/web_camera')
 def web_camera():
     res = Response(compression.gen(compression.WebCam()),
                    mimetype='multipart/x-mixed-replace; boundary=frame')
     return res
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
